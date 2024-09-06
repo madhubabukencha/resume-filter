d4bcf63cc64f9c24e2d116492d7db14e233821de
Madhu Babu Kencha <madhukencha.programmer@gmail.com>
2024-09-06 23:55:33 +0530

    Added home page

--- resume_filter/resume_parser/views.py
+++ resume_filter/resume_parser/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# some_app/views.py
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import datetime
 
-# Create your views here.
+
+class AboutView(LoginRequiredMixin, TemplateView):
+    template_name = "resume_parser/parser_wrapper.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation to get the context
+        context = super().get_context_data(**kwargs)
+        
+        # Add today's date and time to the context
+        context['current_date'] = datetime.now().date()  # for date
+        context['current_time'] = datetime.now().time()  # for time
+
+        return context
+
