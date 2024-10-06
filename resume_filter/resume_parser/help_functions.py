b58a39510a47db18df195685e9116cc6e1524f08
Madhu Babu Kencha <madhukencha.programmer@gmail.com>
2024-10-06 22:26:38 +0530

    Updating a small function

--- resume_filter/resume_parser/help_functions.py
+++ resume_filter/resume_parser/help_functions.py
@@ -11,7 +11,7 @@ import pandas as pd
 import yaml
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ.get("OPENAI"))
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 with open("resume_parser/prompts.yaml", "r", encoding="utf-8") as file:
     data = yaml.safe_load(file)
 
